8019f31b41c17efba0910fc3e09a66e42b43e5d1
Shamim Keshani <sh.keshani@gmail.com>
2024-02-26 17:33:16 +0330

    update

--- lua/shk/lazyness/telescope.lua
+++ lua/shk/lazyness/telescope.lua
@@ -12,6 +12,9 @@ local function config()
 					["<C-j>"] = actions.move_selection_next,               -- move to next result
 					["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
 				},
+				n = {
+					["<C-;>"] = actions.close,
+				}
 			},
 		},
 		pickers = {
