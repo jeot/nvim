ff374d1d41dd05d2fe6c2bfb8efc8773bc4208c6
Shamim Keshani <sh.keshani@gmail.com>
2026-04-21 19:03:31 +0330

    minor change

--- lua/shk/plugins/color.lua
+++ lua/shk/plugins/color.lua
@@ -1,15 +1,15 @@
 return {
-	-- {
-	-- 	"rose-pine/neovim",
-	-- 	as = "rose-pine",
-	-- 	config = function()
-	-- 		require("rose-pine").setup({
-	-- 			disable_background = true,
-	-- 			disable_float_background = true,
-	-- 		})
-	-- 		vim.cmd.colorscheme("rose-pine")
-	-- 	end,
-	-- },
+	{
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				disable_float_background = true,
+			})
+			vim.cmd.colorscheme("rose-pine")
+		end,
+	},
 	-- {
 	-- 	"tanvirtin/monokai.nvim",
 	-- 	config = function()
@@ -20,20 +20,20 @@ return {
 	-- 		vim.cmd.colorscheme("monokai")
 	-- 	end,
 	-- },
-	{
-		"tanvirtin/monokai.nvim",
-		lazy = false, -- load immediately
-		priority = 1000, -- load before other stuff
-		config = function()
-			require("monokai").setup({
-				-- You can customize here if you want (optional)
-				-- palette = require("monokai").pro, -- or .classic, .soda, .ristretto
-				-- other options like italics, etc.
-			})
-
-			vim.cmd.colorscheme("monokai") -- or "monokai_pro", "monokai_soda", etc.
-		end,
-	},
+	-- {
+	-- 	"tanvirtin/monokai.nvim",
+	-- 	lazy = false, -- load immediately
+	-- 	priority = 1000, -- load before other stuff
+	-- 	config = function()
+	-- 		require("monokai").setup({
+	-- 			-- You can customize here if you want (optional)
+	-- 			-- palette = require("monokai").pro, -- or .classic, .soda, .ristretto
+	-- 			-- other options like italics, etc.
+	-- 		})
+	--
+	-- 		vim.cmd.colorscheme("monokai") -- or "monokai_pro", "monokai_soda", etc.
+	-- 	end,
+	-- },
 	-- "rafi/awesome-vim-colorschemes", -- multiple colorscheme in one repository
 	-- "folke/tokyonight.nvim",
 }
